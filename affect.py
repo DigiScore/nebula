563b32e5ce115184d987b9d6db6a0ddc008153e8
craigv <vear2ev2.co.uk>
2022-09-09 12:50:27 +0100

    first build of version 3

--- affect.py
+++ affect.py
@@ -15,7 +15,7 @@ class Affect:
     skeletal tracking, joystick control.
     By analysing the energy of input this class will
     define which feed of the datadict
-    (produced by the AI actory) to emit back to the client.
+    (produced by the AI factory) to emit back to the client.
     Like a thought train it is affected by the energy of a
     percept, and duration of such listening."""
 
@@ -46,37 +46,32 @@ class Affect:
 
     def listener(self):
         """Listens to the realtime incoming signal that is stored in the dataclass ("user_input")
-        and calculates an affectual response based on general bounderies:
+        and calculates an affectual response based on general boundaries:
             HIGH - if input stream is LOUD (0.8+) then emit, smash a random fill and break out to Daddy cycle...
             MEDIUM - if input energy is 0.3-0.8 then emit, a jump out of child loop
             LOW - nothing happens, continues with cycles
         """
         # 1. daddy cycle: top level cycle lasting 6-26 seconds
         while self.running:
-            if self.affect_logging:
-                print('\t\t\t\t\t\t\t\t=========AFFECT - Daddy cycle started ===========')
-
             # flag for breaking on big affect signal
             self.interrupt_bang = True
 
             # Top level calc master cycle before a change
-            master_cycle = randrange(6, 26)  # * self.global_speed
+            master_cycle = randrange(600, 2600) / 100  # * self.global_speed
             loop_end = time() + master_cycle
 
             if self.affect_logging:
-                print(f"                 interrupt_listener: started! sleeping now for {master_cycle}...")
-
-            # refill the dicts
-            self.random_dict_fill()
+                print('\t\t\t\t\t\t\t\t=========AFFECT - Daddy cycle started ===========')
+                print(f"                 interrupt_listener: started! Duration =  {master_cycle} seconds")
 
             # 2. child cycle: waiting for interrupt  from master clock
             while time() < loop_end:
-                if self.affect_logging:
-                    print('\t\t\t\t\t\t\t\t=========Hello - child cycle 1 started ===========')
-
-                # if a major break out then go to Daddy cycle
+                # if a major break out then go to Daddy cycle and restart
                 if not self.interrupt_bang:
                     break
+
+                if self.affect_logging:
+                    print('\t\t\t\t\t\t\t\t=========Hello - child cycle 1 started ===========')
 
                 # randomly pick an input stream for this cycle
                 # either user_in, random, net generation or self-awareness
@@ -84,36 +79,44 @@ class Affect:
                 self.rnd_stream = self.affectnames[rnd]
                 setattr(self.datadict, 'affect_decision', self.rnd_stream)
                 if self.affect_logging:
-                    print(self.rnd_stream)
+                    print(f'Random stream choice = {self.rnd_stream}')
 
                 # hold this stream for 1-4 secs, unless interrupt bang
                 end_time = time() + (randrange(1000, 4000) / 1000)
                 if self.affect_logging:
                     print('end time = ', end_time)
 
-                # baby cycle 2 - own time loops
+                # 3. baby cycle - own time loops
                 while time() < end_time:
-                    # get current mic level
-                    peak = getattr(self.datadict, "user_in")
-                    # print('mic level = ', peak)
-
                     if self.affect_logging:
                         print('\t\t\t\t\t\t\t\t=========Hello - baby cycle 2 ===========')
 
-                    # go get the current value from dict
+                    # make the master output the current value of the affect stream
+                    # 1. go get the current value from dict
                     affect_listen = getattr(self.datadict, self.rnd_stream)
                     if self.affect_logging:
-                        print('current value =', affect_listen)
+                        print(f'Affect stream current input value from {self.rnd_stream} == {affect_listen}')
 
-                    # make the master output the current value of the affect stream
+                    # 2. send to Master Output
                     setattr(self.datadict, 'master_output', affect_listen)
                     if self.affect_logging:
                         print(f'\t\t ==============  master move output = {affect_listen}')
 
-                    # emit to the client at various points in the affect cycle
-                    # self.emitter(affect_listen)
+                    # 3. emit to the client at various points in the affect cycle
+                    self.emitter(affect_listen)
 
-                    # calc affect on behaviour
+                    ###############################################
+                    #
+                    # test realtime input against the affect matrix
+                    # behave as required
+                    #
+                    ###############################################
+
+                    # 1. get current mic level
+                    peak = getattr(self.datadict, "user_in")
+                    print('testing current mic level for affect = ', peak)
+
+                    # 2. calc affect on behaviour
                     # LOUD
                     # if input stream is LOUD then smash a random fill and break out to Daddy cycle...
                     if peak > 0.8:
@@ -123,13 +126,8 @@ class Affect:
                         # A - refill dict with random
                         self.random_dict_fill()
 
-                        # emit at various points in the affect cycle
-                        self.emitter(affect_listen)
-
                         # B - jumps out of this loop into daddy
                         self.interrupt_bang = False
-                        if self.affect_logging:
-                            print('interrupt bang = ', self.interrupt_bang)
 
                         # C break out of this loop, and next (cos of flag)
                         break
@@ -139,18 +137,15 @@ class Affect:
                     elif 0.3 < peak < 0.8:
                         if self.affect_logging:
                             print('interrupt MIDDLE -----------')
-                            print('interrupt bang = ', self.interrupt_bang)
 
-                        # emit at various points in the affect cycle
-                        self.emitter(affect_listen)
-
-                        # jumps out of current local loop, but not main one
+                        # A. jumps out of current local loop, but not main one
                         break
 
                     # LOW
                     # nothing happens here
                     elif peak <= 0.3:
-                        pass
+                        if self.affect_logging:
+                            print('interrupt LOW ----------- no action')
 
                     # get current rhythm_rate from datadict
                     rhythm_rate = getattr(self.datadict, 'rhythm_rate')
@@ -164,6 +159,7 @@ class Affect:
             print(f'//////////////////                   EMITTING value {incoming_affect_listen}')
         self.old_val = incoming_affect_listen
 
+
     def random_dict_fill(self):
         """Fills the working dataclass with random values. Generally called when
         affect energy is highest"""
@@ -171,7 +167,8 @@ class Affect:
             # print(field.name)
             rnd = random()
             setattr(self.datadict, field.name, rnd)
-        print(f'Data dict new random values are = {self.datadict}')
+        if self.affect_logging:
+            print(f'Data dict new random values are = {self.datadict}')
 
     def quit(self):
         self.running = False
